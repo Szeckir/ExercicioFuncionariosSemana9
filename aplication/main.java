24b5f88b549f0edab43d7542d9a26e602f9822ab
Szeckir <80286620+Szeckir@users.noreply.github.com>
2023-10-02 20:02:12 -0300

    Novas atualizacoes

--- aplication/main.java
+++ aplication/main.java
@@ -2,20 +2,17 @@ package aplication;
 import entities.Gerente;
 import java.util.ArrayList;
 import java.util.List;
-
 import entities.Equipe;
 import entities.Funcionario;
 import entities.Vendedor;
 
 
-public class main {
+public class Main {
     public static void main(String[] args) {
-        List<Funcionario> list = new ArrayList<>(); 
-
-        list.add(new Vendedor("Vendedor", 1000, 5, 30));
-        list.add(new Gerente("Gerente", 2000, 10, null, 0));
+        Funcionario v1 = new Vendedor("Joao", 1000, 100, 0);
+        Funcionario g1 = new Gerente("Maria", 2000, 1000, null, 0);
 
         
-       
+
     }
 }
