3f6593002e6a4eac42d62f6c9b516c67762d21ea
Thomaz Szeckir <thomazszeckir@gmail.com>
2023-10-03 10:13:19 -0300

    mudada para letra maiuscula nome classes

--- aplication/main.java
+++ aplication/main.java
@@ -9,7 +9,6 @@ import entities.Vendedor;
 
 public class Main {
     public static void main(String[] args) {
-<<<<<<< HEAD
         Funcionario vendedor1 = new Vendedor("Joao", 1200, 10, 10);
         Funcionario gerente1 = new Gerente("Thomaz", 1200, 10, null, 10);
 
@@ -19,12 +18,5 @@ public class Main {
 
         System.out.println(vendedor1.calculaImposto());
         System.out.println(gerente1.calculaImposto());
-=======
-        Funcionario v1 = new Vendedor("Joao", 1000, 100, 0);
-        Funcionario g1 = new Gerente("Maria", 2000, 1000, null, 0);
-
-        
-
->>>>>>> 24b5f88b549f0edab43d7542d9a26e602f9822ab
     }
 }
